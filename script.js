7cfeb7ccb1fbed84a6053691c89c6662363a4d8a
MartynenkoDV <MartynenkoDV@comfy.ua>
2022-06-03 23:40:02 +0300

    update HW

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 // ------------- # 1
-async function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+async function delay(data, ms) {
+    return new Promise(resolve => setTimeout(() => resolve(data), ms));
 }
 
 async function getUserInfo() {
-    await delay(1000);
-    return { name: 'Vic', age: 21, id: 1 };
+    return await delay({ name: 'Vic', age: 21, id: 1 },1000);
 }
 
 async function getUserAvatar(userInfo) {
-    userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg';
-    await delay(1000);
-
-    return userInfo;
+    await delay(
+        userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg',
+        1000
+    );
 }
 
 async function getUserAdditionalInfo(userInfo) {
-    userInfo.interests = ['sport', 'books'];
-    await delay(1000);
-
-    return userInfo;
+    await delay(userInfo.interests = ['sport', 'books'], 1000);
 }
 
 async function getResult() {
@@ -36,6 +32,7 @@ getResult();
 async function getUser() {
     return { name: 'Vic', age: 21, id: 1 };
 }
+
 async function getInfo() {
     try {
         let user = await getUser();
